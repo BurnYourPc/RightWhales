723215500a8442f682987f8159be0fb43dac2df0
dchrist500 <35398144+dchrist500@users.noreply.github.com>
2018-01-13 15:46:03 +0200

    read csv

--- WhalesData.py
+++ WhalesData.py
@@ -1,3 +1,5 @@
+import csv
+import os
 
 class WhalesData:
 
@@ -5,9 +7,18 @@ class WhalesData:
 
     def __init__(self, data_base):
         self.create_path(data_base)
+        self.read_csv()
 
     def create_path(self, data_base):
         self.data_base = data_base
+
+    def read_csv(self):
+        oos = os.name
+        if oos == 'nt':
+            base = self.data_base + '\\train.csv\\train.csv'
+        else:
+            base = self.data_base + '/train.csv/train.csv'
+        print(base)
 
     def get_path(self):
         return self.data_base
